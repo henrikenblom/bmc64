533f4e3fa3e796959e315f54cfb032f50b68f577
Henrik Enblom <henblom@spotify.com>
2024-02-07 21:39:36 +0100

    Try something simpler

--- third_party/common/kbd.c
+++ third_party/common/kbd.c
@@ -412,8 +412,7 @@ void emu_key_pressed(long key) {
   }
 
   if (key == KEYCODE_KP_Subtract) {
-    emux_last_key_interrupt(0);
-    return;
+    emux_key_interrupt(KEYCODE_Space, 1);
   }
 
   if (ui_enabled) {
@@ -472,8 +471,7 @@ void emu_key_released(long key) {
   }
 
   if (key == KEYCODE_KP_Subtract) {
-    emux_last_key_interrupt(0);
-    return;
+    emux_key_interrupt(KEYCODE_Space, 0);
   }
 
   // Intercept keys meant to become joystick values
