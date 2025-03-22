07b611ac2dfb6a528a8317f0af4b8f7be4cfa5aa
Henrik Enblom <henrik.enblom@enblom.com>
2025-03-22 22:20:53 +0100

    Re-add the all-zero check in circle_sound_write

--- kernel.cpp
+++ kernel.cpp
@@ -1177,10 +1177,32 @@ int CKernel::circle_sound_init(const char *param, int *speed, int *fragsize,
   return 0;
 }
 
+int isallz(char *buf, size_t len)
+{
+  long *wide;
+
+  while ((size_t)buf & (sizeof(long) - 1))
+    if (len--, *buf++)
+      return 0;
+  wide = (long *)buf;
+  while (len >= sizeof(long))
+    if (len -= sizeof(long), *wide++)
+      return 0;
+  buf = (char *)wide;
+  while (len)
+    if (len--, *buf++)
+      return 0;
+  return 1;
+}
+
 // Called from VICE: Core 1
 int CKernel::circle_sound_write(int16_t *pbuf, size_t nr) {
   if (mViceSound) {
-    circle_set_aux_gpio(1, 1);
+    if (isallz((char *)pbuf, nr)) {
+      circle_set_aux_gpio(1, 0);
+    } else {
+      circle_set_aux_gpio(1, 0);
+    }
     return mViceSound->AddChunk(pbuf, nr);
   }
   return 0;
